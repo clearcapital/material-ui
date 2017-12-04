781fce8ee5917760111e90f0133c4a0b353c1b0a
Eric Martin <eric@ericmmartin.com>
2017-12-04 12:45:53 -0800

    Fix build script to use new name

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -58,7 +58,7 @@ function createPackageFile() {
     } = packageData;
 
     const minimalPackage = {
-      name: 'material-ui',
+      name: 'cc-material-ui',
       author,
       version,
       description,
